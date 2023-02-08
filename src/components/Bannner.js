755580eb63ec74ab464e0574ae0481c1ceff2b53
King Amir <radioestekhdam@gmail.com>
2023-02-08 13:28:05 +0330

    change stack

--- src/components/Bannner.js
+++ src/components/Bannner.js
@@ -11,12 +11,14 @@ const Bannner = () => {
             <div className={styles.bannerbox}>
             <img className='banner' src={banner} alt='baneer' />
             </div>
-            {/* <div className={styles.textContainer}>
+            {
+            /* <div className={styles.textContainer}>
                 <h1>فروشگاه</h1>
                 <p>
                     فروشگاهی به وسعت <span>ایران زمین!</span>
                     </p>
-            </div> */}
+            </div> */
+            }
          
         </div>
          
